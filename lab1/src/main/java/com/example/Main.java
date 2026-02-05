fe54342073978aa898ee8bd0e5a582e60bca2ef7
msheliaha <msheliaha@griddynamics.com>
2026-02-05 13:54:17 +0200

    Implement class for calculation

--- lab1/src/main/java/com/example/Main.java
+++ lab1/src/main/java/com/example/Main.java
@@ -1,20 +1,36 @@
 package com.example;
 
+import com.example.task.Solution;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Mode;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        org.openjdk.jmh.Main.main(args);
+        Solution solution = new Solution();
+        var matrixA = solution.generateMatrix(10);
+        var matrixB = solution.generateMatrix(10);
+        int k = 1;
+
+        System.out.println(Arrays.deepToString(matrixA));
+        System.out.println(Arrays.deepToString(matrixB));
+
+        var result = solution.executeSequentially(matrixA, matrixB, k);
+        System.out.println(Arrays.deepToString(result));
+
+        var parallelResult = solution.executeParallel(matrixA, matrixB, k, 10);
+        System.out.println(Arrays.deepToString(parallelResult));
+
+
+
+        //org.openjdk.jmh.Main.main(args);
     }
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    @Fork(value = 5, warmups = 5)
     public void init() {
         // Do nothing
         int i = 10;
---
+++ lab1/src/main/java/com/example/task/Solution.java
@@ -0,0 +1,79 @@
+package com.example.task;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+// Task: C = A + k*B
+public class Solution {
+
+    public int[][] generateMatrix(int size){
+        Random random = new Random();
+        var matrix = new int[size][size];
+
+        for(int[] row: matrix){
+            for(int i = 0; i<row.length;i++){
+                row[i] = random.nextInt(100);
+            }
+        }
+        return matrix;
+    }
+
+    public int[][] executeSequentially(int[][] a, int[][] b, int k) {
+        if(a==null || b==null || a.length!=b.length || a.length<1) throw new IllegalArgumentException();
+        int rows = a.length;
+        int cols = a[0].length;
+
+        int[][] result = new int[rows][cols];
+
+        for(int i = 0; i<rows; i++){
+            for(int j = 0; j<cols; j++){
+                result[i][j] = a[i][j] + k*b[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    public int[][] executeParallel(int[][] a, int[][] b, int k, int threadNum){
+        if(a==null || b==null || a.length!=b.length || a.length<1) throw new IllegalArgumentException();
+        if(threadNum < 1) throw new IllegalArgumentException();
+
+        List<Thread> threads = new ArrayList<>();
+        int rows = a.length;
+        int cols = a[0].length;
+        if(threadNum > rows) threadNum = rows;
+
+        int rowsPerThread = rows/threadNum;
+
+        int[][] result = new int[rows][cols];
+        for(int row = 0; row<rows; row+=rowsPerThread) {
+            int lastRow = Math.min(rows, row+rowsPerThread);
+            Thread thread = createThread(a, b, k, result, row, lastRow);
+            thread.start();
+            threads.add(thread);
+        }
+
+        threads.forEach(t-> {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        return result;
+    }
+
+    private Thread createThread(int[][] a, int[][] b, int k, int[][] buffer,  final int firstRow, final int lastRow){
+        return new Thread(()->{
+            int cols = buffer[firstRow].length;
+            for(int i = firstRow; i<lastRow; i++){
+                for(int j = 0; j<cols; j++){
+                    buffer[i][j] = a[i][j] + k*b[i][j];
+                }
+            }
+        });
+    }
+
+}
