4a6e53cb81ce8435240d642d8c73394407334c68
msheliaha <msheliaha@griddynamics.com>
2026-02-05 09:47:08 +0200

    Init project for lab1

---
+++ lab1/src/main/java/com/example/Main.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Mode;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        org.openjdk.jmh.Main.main(args);
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @Fork(value = 5, warmups = 5)
+    public void init() {
+        // Do nothing
+        int i = 10;
+        int j = 2005;
+        int c = i+j;
+    }
+}
